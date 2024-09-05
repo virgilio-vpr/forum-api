17f71914dac2d729982e9ef4b1b99a3d5c3ed096
virgilio-vpr Rosa <virgilio.vpr@gmail.com>
2024-09-05 19:36:23 +0000

    Adding User Authtentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Inject()
+    private readonly authService: AuthService;
+
+    @Post('signin')
+    signin(@Body() body: Prisma.UserCreateInput) {
+        return this.authService.signin(body);
+    }
+}
